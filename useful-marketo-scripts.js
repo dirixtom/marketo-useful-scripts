315eea30905b8cc4ed8d29e9b392fa5085e0586c
Tom Dirix <dirixtom@gmail.com>
2019-01-25 08:19:59 +0100

    Update useful-marketo-scripts.js
    + uncheck = pref center unsub support

--- useful-marketo-scripts.js
+++ useful-marketo-scripts.js
@@ -1,13 +1,52 @@
-/* ! marketo-useful-scripts.js v0.0.1 */
+/* ! marketo-useful-scripts.js v0.2.0 | MIT License | https://github.com/dirixtom/marketo-useful-scripts.js*/
 
 // If ${ABC} is defined in comments, where ABC can be whatever, a Marketo String Variable is recommended
 
-// Redirect if not on certain page eg. inside of Marketo LP editor (marketodesigner)
-// Invoke with redirect("marketodesigner", "${url}", ${time})
-function redirect(e, p, t) {
-	if (document.location.href.indexOf(e) === -1){
+// Hard redirect if not on certain page
+// replace("marketodesigner", "${url}", ${time});
+function replace(s, u, t) {
+	if (document.location.href.indexOf(s) === -1){
 		window.setTimeout(function(){
-				window.location.replace(p);
+			window.location.replace(u);
 		}, t);
 	}
 }
+
+// Soft redirect if not on certain page
+// redirect("marketodesigner", "${url}", ${time});
+function redirect(s, u, t) {
+	if (document.location.href.indexOf(s) === -1){
+		window.setTimeout(function(){
+			window.location.href = u;
+		}, t);
+	}
+}
+
+// Preference center unsubscribe checkbox functionality, unchecks all checkboxes other when unsubscribe is checked
+function uncheck(u) {
+	let checkboxes = document.querySelectorAll('input[type=checkbox]:not([name=' + u + '])'),
+			unsubInput = document.querySelector('input[name=' + u + ']');
+
+	for (i = 0; i < checkboxes.length; i++) {
+		checkboxes[i].addEventListener("click", function() {
+			if (this.checked == true) {
+				unsubInput.checked = false;
+			}
+		});
+	}
+
+	unsubInput.onchange = function() {
+		if (this.checked) {
+			let inputs = document.getElementsByTagName("input");
+			for (index = 0; index < inputs.length; ++index) {
+				if (inputs[index].type == "checkbox" && inputs[index].name != u) {
+						inputs[index].checked = false;
+				}
+			}
+		}
+	};
+}
+
+MktoForms2.whenReady(function(form) {
+	uncheck("Unsubscribed");
+});
